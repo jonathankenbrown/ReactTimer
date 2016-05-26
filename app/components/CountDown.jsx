071075c1e854ddb9f9c24e36e369480579d27c06
Jonathan Brown <jonathankenbrown@gmail.com>
2016-05-25 22:37:26 -0700

    Finalize tests & add page title

--- app/components/CountDown.jsx
+++ app/components/CountDown.jsx
@@ -73,6 +73,7 @@ var CountDown = React.createClass({
 
     return (
       <div>
+        <h1 className="page-title">Countdown App</h1>
         <Clock totalSeconds={count}/>
         {renderControlArea()}
       </div>
